4c347e321922f7c08781fda1c004f5b20007691c
Justin Arnold <justin.arnold@programmer.net>
2024-09-12 23:55:27 -0400

    feat: Added support to add new services/links

--- components/AppLinkAddModal.vue
+++ components/AppLinkAddModal.vue
@@ -1,7 +1,23 @@
 <script lang="ts" setup>
+import { z } from "zod"
+import { toTypedSchema } from '@vee-validate/zod';
+import { useStorage } from '@vueuse/core'
+
 const isOpen = defineModel()
 
-const modal = ref()
+const modal = ref<HTMLDialogElement>()
+
+const addServiceValidationSchema = toTypedSchema(z.object({
+    name: z.string().min(1, { message: 'This is required' }),
+    address: z.string().min(1, { message: 'This is required' })
+}))
+
+const { errors, handleSubmit, defineField } = useForm({
+  validationSchema: addServiceValidationSchema,   
+});
+
+const [name, nameAttributes] = defineField('name');
+const [address, addressAttributes] = defineField('address');
 
 watchEffect(() => {
     if (isOpen.value) {
@@ -12,23 +28,45 @@ watchEffect(() => {
 })
 
 function openModal() {
-    modal.value.showModal()
+    modal.value?.showModal()
 }
 
 function closeModal() {
-    modal.value.closeModal()
+    modal.value?.close()
 }
+
+const addService = handleSubmit(values => {
+    const links = useStorage<{name: string, address: string}[]>('cerebral-link-items', [])
+    links.value.push(values)
+    closeModal()
+});
+
+
 </script>
 <template>
     <dialog ref="modal" modal class="modal backdrop:bg-black/30 backdrop:backdrop-blur-md" @close="isOpen = false" >
         <div class="modal-box">
-            <h3 class="text-lg font-bold">Hello!</h3>
-            <p class="py-4">Press ESC key or click the button below to close</p>
+            <h3 class="text-lg font-bold">Add a new service</h3>
             <div class="modal-action">
-            <form method="dialog">
-                <!-- if there is a button in form, it will close the modal -->
-                <button class="btn">Close</button>
-            </form>
+                <form method="dialog" class="form-control w-full gap-6" @submit="addService">
+                    <div>
+                        <label class="flex flex-col gap-1">
+                            Name
+                            <input class="input input-primary" v-model="name" v-bind="nameAttributes" />
+                        </label>
+                        <div>{{ errors.name }}</div>
+                    </div>
+                    <div>
+                        <label class="flex flex-col gap-1">
+                            Address
+                            <input class="input input-primary" v-model="address" v-bind="addressAttributes"/>
+                        </label>
+                        <div>{{ errors.address }}</div>
+                    </div>
+                    <button class="btn btn-outline mt-2">
+                        Add
+                    </button>
+                </form>
             </div>
         </div>
     </dialog>
--- pages/app.vue
+++ pages/app.vue
@@ -7,12 +7,12 @@ definePageMeta({
 
 const isOpen = ref(false)
 
-const links = useStorage<{name: string, url: string}[]>('cerebral-link-items', [])
+const links = useStorage<{name: string, address: string}[]>('cerebral-link-items', [])
 const hasLinks = computed(() => links.value.length)
 </script>
 
 <template>
-    <AppLinkCard v-for="each, index in links" :key="index" :to="each.url">
+    <AppLinkCard v-for="each, index in links" :key="index" :to="each.address">
         {{ each.name }}
     </AppLinkCard>
     <div v-if="!hasLinks" class="fixed top-0 left-0 h-screen w-screen grid place-items-center">
