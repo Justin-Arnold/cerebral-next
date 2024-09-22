889e3ae697e5df9ee4b5c5012248396f3f704513
Justin Arnold <justin.arnold@programmer.net>
2024-09-21 22:42:10 -0400

    feat: Adjust landing header

--- components/LandingHeader.vue
+++ components/LandingHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <CerebralHeader class="fixed top-0 left-0">
-        <template #header-menu>
+        <template #header-actions>
             <NuxtLink to="/app" class="btn btn-neutral px-6">
                     Login
             </NuxtLink>
