009fd190f36e2266dab4d66c90722dcd30105233
Justin Arnold <justin.arnold@programmer.net>
2024-10-09 19:13:13 -0400

    feat: Format link to open when protocol is not provided

--- components/AppLinkCard.vue
+++ components/AppLinkCard.vue
@@ -10,10 +10,18 @@ const links = useAppLinks()
 function onDeleteClick() {
     links.remove(props.link)
 }
+
+const formattedLink = computed(() => {
+  let url = props.link.address;
+  if (!url.startsWith('http://') && !url.startsWith('https://')) {
+    url = 'https://' + url;
+  }
+  return url;
+});
 </script>
 
 <template>
-    <NuxtLink class="card bg-base-300/80 w-60 h-40 shadow-md shadow-base-200 group" :to="link.address" target="_blank" rel="noopener">
+    <NuxtLink class="card bg-base-300/80 w-60 h-40 shadow-md shadow-base-200 group" :to="formattedLink" target="_blank" rel="noopener">
         <div class="absolute top-0 left-0 h-full w-full grid place-items-center z-[1]">
             <slot></slot>
         </div>
