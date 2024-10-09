688b6753ebfe74297287d64c62e2dad79f4c3802
Justin Arnold <justin.arnold@programmer.net>
2024-10-08 22:35:45 -0400

    feat: Add text to add service button

--- components/AppLinkAddButton.vue
+++ components/AppLinkAddButton.vue
@@ -9,7 +9,8 @@ const isOpen = ref(false)
 </script>
 
 <template>
-    <button class="btn text-primary bg-neutral/80 rounded-full aspect-square" :class="{'btn-ghost': isGhost}" @click="isOpen = true">
+    <button class="btn text-primary bg-neutral/80 rounded-full aspect-square flex gap-2 flex-nowrap text-nowrap" :class="{'btn-ghost': isGhost}" @click="isOpen = true">
+        Add Service
         <Icon name="mingcute:add-fill" class="text-xl"/>
     </button>
     <AppLinkAddModal v-model="isOpen"/>
